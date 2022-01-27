6c7f4232cec97985de62c10fc1b6d1a3489a2735
Destileus <danielaraneva7@gmail.com>
2022-01-27 12:09:30 +0000

    More changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 // App.tsx
 import React from 'react'
-import MyFirstComponent from './components/MyFirstComponent';
+import MyFirstComponent from './components/MyFirstComponent'
+import SomeExampleComponent from './components/example/someExampleComponent';
+
 
 const App: React.FC = () => {
   return (
     <div>
       <MyFirstComponent />
+      <SomeExampleComponent />
     </div>
   )
 }
