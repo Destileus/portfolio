6611fc50c5f23c7ab823e7aec329a3c58bd00085
Destileus <danielaraneva7@gmail.com>
2022-03-07 07:29:44 +0000

    Removed things

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 // App.tsx
 import React from 'react'
 import styles from './App.module.scss'
-import MyFirstComponent from './components/MyFirstComponent'
-import SomeExampleComponent from './components/example/someExampleComponent';
-import ClickCount from './components/clickCount';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from 'react-router-dom';
 import Main from './components/sites/main/main';
 import Projects from './components/sites/projects/projects';
 import Cv from './components/sites/cv/cv';
 import Navbar from './components/navbar/navbar';
-
-type AppProps = {
-
-}
 
 const App: React.FC = () => {
   return (
