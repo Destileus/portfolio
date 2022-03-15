be7c128f343c790162dd89fbfb2f1d8f292401c0
Destileus <danielaraneva7@gmail.com>
2022-03-15 10:12:16 +0000

    fixes

--- src/components/sites/main/main.tsx
+++ src/components/sites/main/main.tsx
@@ -11,7 +11,7 @@ const Main: React.FC<MainProps> = () => {
     <div className={styles.container}>
       <h1>Daniel Araneva</h1>
       <p>Not a professional, I just write code, and sometimes it works.</p>
-      <img src={bigheadimg} alt='picture not found lol'/>
+      <img src={bigheadimg} alt='not found lol'/>
     </div>
   )
 }
