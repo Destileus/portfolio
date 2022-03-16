e9c73b7aaeddae0b03b20457e5811d0fe7029f70
Destileus <danielaraneva7@gmail.com>
2022-03-16 09:11:12 +0000

    whoops

--- src/components/sites/projects/projects.tsx
+++ src/components/sites/projects/projects.tsx
@@ -10,9 +10,9 @@ const Projects: React.FC<ProjectsProps> = () => {
   return (
     <div className={styles.container}>
       <p>Here you can check out all the projects I've been working on.</p>
-      <a href="https://www.youtube.com/watch?v=F_TV4vZRSE8&list=PL-CmxXOSdR4Yx29FuG4__hcb1qW3YzsEt&index=5" target='_blank' rel='noreferrer'>Cool song</a>
       
-      <div className={styles.projectsContainer}>
+
+        <div className={styles.projectsContainer}>
         <UnityGameProject embedURL='https://play.unity3dusercontent.com/webgl/ce528440-1925-406c-bfd7-34755a8ed2fa?screenshot=false&embedType=embed' />
       </div>
 
