7637ce1fe8cc9b5348764e23581bfb926e5cb04e
Destileus <danielaraneva7@gmail.com>
2022-03-17 07:56:31 +0000

    text

--- src/components/sites/main/main.tsx
+++ src/components/sites/main/main.tsx
@@ -12,6 +12,7 @@ const Main: React.FC<MainProps> = () => {
       <h1>Daniel Araneva</h1>
       <p>I develop things. Been doing that for a while now.</p>
       <p>Pay respect for the mighty big head.</p>
+      
       <img src={bigheadimg} alt='not found lol'/>
     </div>
   )
